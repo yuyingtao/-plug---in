d58fa22d70c80d096fe0f2eaa994034422750a71
yuyingtao <2221894222@qq.com>
2018-08-16 15:22:40 +0800

    有序预加载

--- js/imgPreload.js
+++ js/imgPreload.js
@@ -4,13 +4,40 @@
 	function Preload(imgs,opt){
 		this.imgs=(typeof imgs ==='string') ? [imgs] : imgs;
 		this.opts=$.extend({},Preload.init,opt);
-    this._unorderLoad()
+		if(this.opts.order==true){
+			this._orderLoad();
+		}else {
+			this.opts._unorderLoad();
+		}   
 	}
+	//默认数据
 	Preload.init={
+		order:false,
 		eachF:null,
 		loaded:null
 	}
-	Preload.prototype._unorderLoad=function(){
+	Preload.prototype._orderLoad=function() { //有序预加载
+		var count=0,
+		    img=this.imgs,
+		    len=img.length,
+		    opts=this.opts;   
+		function load() {
+			  opts.eachF&&opts.eachF(count);
+		    	var imgObj=new Image();
+		    	$(imgObj).on('error load', function () {
+		    		if (count >=len) {
+		    			opts.loaded&&opts.loaded();
+		    		} else {
+		    			load();
+		    		}
+		    		count++;
+		    		
+		    	})
+		    	imgObj.src=img[count];
+		    }
+		 load();
+	}
+	Preload.prototype._unorderLoad=function() { //无序预加载
 		var count=0,
 		    img=this.imgs,
 		    len=img.length,
